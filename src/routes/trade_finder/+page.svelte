85db357bdae27134f417c2838a4608a421859645
spenceh14 <38918096+spenceh14@users.noreply.github.com>
2024-06-30 16:19:56 -0400

    trying to click ktc span

--- src/routes/trade_finder/+page.svelte
+++ src/routes/trade_finder/+page.svelte
@@ -2,17 +2,22 @@
   import { onMount } from 'svelte';
 
   let iframe;
-  
+
   onMount(() => {
     iframe.onload = () => {
       try {
         const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-        const button = iframeDocument.getElementById('dont-know');
-        if (button) {
-          button.click();
-        } else {
-          console.error('Button not found in the iframe.');
-        }
+
+        // Wait for the DOM inside the iframe to be fully loaded
+        const checkButton = setInterval(() => {
+          const button = iframeDocument.querySelector('span#dont-know');
+          if (button) {
+            button.click();
+            clearInterval(checkButton);
+          } else {
+            console.error('Button not found in the iframe.');
+          }
+        }, 1000); // Check every second
       } catch (error) {
         console.error('Error accessing iframe content:', error);
       }
@@ -20,6 +25,11 @@
   });
 </script>
 <div class="holder">
-<iframe bind:this={iframe} src="https://keeptradecut.com/dynasty/power-rankings/league-overview?leagueId=1048240987706941440&platform=2" id="ktc-rankings" style="width: 800px; height: 600px;"/>
+<iframe 
+  bind:this={iframe} 
+  src=https://keeptradecut.com/dynasty/power-rankings/league-overview?leagueId=1048240987706941440&platform=2" 
+  id="ktc-pos-rank-iframe" 
+  style="width: 800px; height: 600px; overflow: auto;" 
+/>
 
 </div>
