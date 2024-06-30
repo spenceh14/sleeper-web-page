60d888d58d1bed72c4e1880325fc0f5fd4414581
spenceh14 <38918096+spenceh14@users.noreply.github.com>
2024-06-30 16:10:06 -0400

    click dontknow in KTC

--- src/routes/trade_finder/+page.svelte
+++ src/routes/trade_finder/+page.svelte
@@ -1,4 +1,25 @@
+<script>
+  import { onMount } from 'svelte';
 
+  let iframe;
+  
+  onMount(() => {
+    iframe.onload = () => {
+      try {
+        const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+        const button = iframeDocument.getElementById('dont-know');
+        if (button) {
+          button.click();
+        } else {
+          console.error('Button not found in the iframe.');
+        }
+      } catch (error) {
+        console.error('Error accessing iframe content:', error);
+      }
+    };
+  });
+</script>
 <div class="holder">
-<iframe src="https://keeptradecut.com/dynasty/power-rankings/league-overview?leagueId=1048240987706941440&platform=2"title="description"></iframe>
+<iframe bind:this={iframe} src="https://keeptradecut.com/dynasty/power-rankings/league-overview?leagueId=1048240987706941440&platform=2" id="ktc-rankings" />
+
 </div>
