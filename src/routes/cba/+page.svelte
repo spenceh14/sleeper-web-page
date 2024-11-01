b88fe47e5f178121df7b8d6f3c8c1d7908219d1b
spenceh14 <spencerjhaymond@gmail.com>
2024-10-31 20:14:16 -0400

    CBA updates

--- src/routes/cba/+page.svelte
+++ src/routes/cba/+page.svelte
@@ -4,35 +4,38 @@
 
 
 <h1> Current CBA </h1>
-<p>
-Regular season settings:
-Waiver Order- FAAB Bidding
-Waiver Budget $125
-After games waivers clear 3am Wednesday
-Time to review pending trades, 1 day
-Trade Deadline, week 11
-2 IR Spots
-Current Auto Sub is time sensitive
-Allow moves pre-draft
-Taxi Squad deadline is start of regular season
-currently OFF- 'Prevent Bench Players from being dropped after game starts'
-Trades- as soon as majority votes 'pass', trades are processed
-
-2024 Payouts:
-Champion- $1000
-Runner up- $200
-Highest scorer (regular season)- $25/week
-Highest scoring loser (regular season)- $25/week
-Toilet Bowl 'winner'- $50
-3rd place- $75
-4th place- $50
-Decade Pot- $300
-
-Annual Calendar:
-Offseason Waivers Open up June 1st
-Rookie draft- Sunday in August after preseason games
-Trade deadline- Week 11
-
-Offseason Settings:
+<div>
+<h2> Regular season settings</h2>
+<ul>
+<li>Waiver Order- FAAB Bidding</li>
+<li>Waiver Budget $125</li>
+<li>After games waivers clear 3am Wednesday</li>
+<li>Time to review pending trades, 1 day</li>
+<li>Trade Deadline, week 11</li>
+<li>2 IR Spots</li>
+<li>Current Auto Sub is time sensitive</li>
+<li>Allow moves pre-draft</li>
+<li>Taxi Squad deadline is start of regular season</li>
+<li>currently OFF- 'Prevent Bench Players from being dropped after game starts'</li>
+<li>Trades- as soon as majority votes 'pass', trades are processed</li>
+</ul>
+<h2> 2024 Payout Structure</h2>
+<ul>
+<li>Champion- $1000</li>
+<li>Runner up- $200</li>
+<li>Highest scorer (regular season)- $25/week</li>
+<li>Highest scoring loser (regular season)- $25/week</li>
+<li>Toilet Bowl 'winner'- $50</li>
+<li>3rd place- $75</li>
+<li>4th place- $50</li>
+<li>Decade Pot- $300</li>
+</ul>
+<h2> Annual Calendar</h2>
+<ul>
+<li>Offseason Waivers Open up June 1st</li>
+<li>Rookie draft- Sunday in August after preseason games</li>
+<li>Trade deadline- Week 11</li>
+</ul>
+<h2>Offseason Settings</h2>
 Waivers are set to custom daily waivers, 'Waivers to FA'
-</p>
+</div>
