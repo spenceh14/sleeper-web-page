e5d2271051a9a4d7e9fd5cab13446a09100aa21c
spenceh14 <spencerjhaymond@gmail.com>
2024-10-31 20:07:13 -0400

    CBA updates

--- src/routes/cba/+page.svelte
+++ src/routes/cba/+page.svelte
@@ -1,1 +1,38 @@
 <p>CBA Topics </p>
+
+
+
+
+<h1> Current CBA </h1>
+<p>
+Regular season settings:
+Waiver Order- FAAB Bidding
+Waiver Budget $125
+After games waivers clear 3am Wednesday
+Time to review pending trades, 1 day
+Trade Deadline, week 11
+2 IR Spots
+Current Auto Sub is time sensitive
+Allow moves pre-draft
+Taxi Squad deadline is start of regular season
+currently OFF- 'Prevent Bench Players from being dropped after game starts'
+Trades- as soon as majority votes 'pass', trades are processed
+
+2024 Payouts:
+Champion- $1000
+Runner up- $200
+Highest scorer (regular season)- $25/week
+Highest scoring loser (regular season)- $25/week
+Toilet Bowl 'winner'- $50
+3rd place- $75
+4th place- $50
+Decade Pot- $300
+
+Annual Calendar:
+Offseason Waivers Open up June 1st
+Rookie draft- Sunday in August after preseason games
+Trade deadline- Week 11
+
+Offseason Settings:
+Waivers are set to custom daily waivers, 'Waivers to FA'
+</p>
